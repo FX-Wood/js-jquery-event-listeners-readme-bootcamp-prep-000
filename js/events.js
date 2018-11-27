0945cf0908ffef680646f4562c0444fd8eff65e1
FX Wood <fxwood2@gmail.com>
2018-11-27 01:43:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,7 +4,9 @@ function getIt()  {
 function frameIt()  {
   $('img').on("load", function() {$('img').addClass("tasty")})
 }
-
+function addAClass(element, classToAdd)  {
+  $(`${element}`).addClass(`${classToAdd}`)
+}
 $(document).ready(function(){
   getIt()
   frameIt()
