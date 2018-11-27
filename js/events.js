bd220cdf395648007c231415279e896828537cf9
FX Wood <fxwood2@gmail.com>
2018-11-27 01:36:55 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,7 @@
 //define functions here
-
+function getIt()  {
+  $('p').on("click", (function() {alert("Hey!")})
+}
 
 $(document).ready(function(){
   $('h1').on("clicked", function() {alert("Hey!")})
